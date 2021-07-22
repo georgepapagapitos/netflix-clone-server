3b55ba33cd08558f56c16f728ca572b327fd3441
George Papagapitos <george.g.papagapitos@gmail.com>
2021-07-22 10:23:00 -0500

    list router working

--- routes/lists.js
+++ routes/lists.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { findByIdAndDelete } = require("../models/List");
 const List = require("../models/List");
 const verifyToken = require("../verifyToken");
 
@@ -19,7 +20,43 @@ router.post("/", verifyToken, async (req, res) => {
 
 // DELETE
 router.delete("/:id", verifyToken, async (req, res) => {
-
+  if (req.user.isAdmin) {
+    try {
+      await List.findByIdAndDelete(req.params.id);
+      res.status(201).json("List has been deleted");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("Not authorized");
+  }
 });
 
-// GET 
+// GET
+router.get("/", async (req, res) => {
+  const type = req.query.type;
+  const genre = req.query.genre;
+  let list = [];
+  try {
+    if (type) {
+      if (genre) {
+        list = await List.aggregate([
+          { $sample: { size: 10 } },
+          { $match: { type: type, genre: genre } },
+        ]);
+      } else {
+        list = await List.aggregate([
+          { $sample: { size: 10 } },
+          { $match: { type: type } },
+        ]);
+      }
+    } else {
+      list = await List.aggregate([{ $sample: { size: 10 } }]);
+    }
+    res.status(200).json(list);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
